16a4f11e31399c7787ed6b9e8163c477af8077a1
0MAVM0 <ediconpts9@gmail.com>
2025-06-27 19:08:20 +0500

    Urls Of Profiles; App - users

--- config/users/urls.py
+++ config/users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import RegisterView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
+from rest_framework_simplejwt.views import ( TokenObtainPairView, TokenRefreshView )
+from .views import RegisterView, ProfileView, UserPublicProfileView
 
 urlpatterns = [
+    # Auth
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Profile
+    path('me/', ProfileView.as_view(), name='me'),
+    path('<int:pk>/', UserPublicProfileView.as_view(), name='user-profile'),
 ]
