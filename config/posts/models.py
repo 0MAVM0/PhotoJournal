59f6087c7b37bcd0f56d1a1c1e7f405eec8eac99
0MAVM0 <ediconpts9@gmail.com>
2025-06-27 20:15:13 +0500

    Model - Post; App - posts

--- config/posts/models.py
+++ config/posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/')
+    caption = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
