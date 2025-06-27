6d05cc8f57674cc8735c353a7f9353871d71e457
0MAVM0 <ediconpts9@gmail.com>
2025-06-27 19:04:16 +0500

    Views Of Profiles - ProfileView & UserPublicProfileView; App - users

--- config/users/views.py
+++ config/users/views.py
@@ -1,6 +1,7 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+
+from .serializers import RegisterSerializer, UserProfileSerializer, UserUpdateSerializer
 from .models import CustomUser
 
 
@@ -8,3 +9,18 @@ class RegisterView(CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [ AllowAny ]
+
+
+class ProfileView(RetrieveUpdateAPIView):
+    def get_object(self):
+        return self.request.user
+    
+    def get_serializer_class(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return UserUpdateSerializer
+        return UserProfileSerializer
+
+
+class UserPublicProfileView(RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserProfileSerializer
