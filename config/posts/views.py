8d2c565b5cb7d7819717c56d8c94b0ddb2c83f5a
0MAVM0 <ediconpts9@gmail.com>
2025-06-27 20:52:03 +0500

    Views - PostListCreateView & PostRetrieveUpdateDestroyView; App - posts

--- config/posts/views.py
+++ config/posts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.core.exceptions import PermissionDenied
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostListCreateView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_update(self, serializer):
+        if self.request.user != self.get_object().user:
+            raise PermissionDenied('You do not have permission to edit this post.')
+        serializer.save()
+    
+    def perform_destroy(self, instance):
+        if self.request.user != instance.user:
+            raise PermissionDenied('You do not have permission to delete this post.')
+        instance.delete()
