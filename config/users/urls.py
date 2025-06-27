043830888b213c55a4bd136801365dcc9688ab3b
0MAVM0 <ediconpts9@gmail.com>
2025-06-27 13:13:00 +0500

    Urls Of Auth; App - users

--- config/users/urls.py
+++ config/users/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
-from .views import *
+from .views import RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', TokenRefreshView.as_view(), name='token_refresh'),
+]
