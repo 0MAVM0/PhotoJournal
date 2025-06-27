ab10140d995bb7d605df0b08f68cfce03b2abe0d
0MAVM0 <ediconpts9@gmail.com>
2025-06-27 18:18:22 +0500

    Fixing A Bug With Permissions In A View - RegisterView

--- config/users/views.py
+++ config/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
 from .models import CustomUser
 
@@ -6,3 +7,4 @@ from .models import CustomUser
 class RegisterView(CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [ AllowAny ]
