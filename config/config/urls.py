5bc5d5330d64e38ce3987bbd170d441b27a9478d
0MAVM0 <ediconpts9@gmail.com>
2025-06-29 12:25:07 +0500

    Enabling Everyone To Read Docs From Swagger

--- config/config/urls.py
+++ config/config/urls.py
@@ -6,6 +6,8 @@ from django.contrib import admin
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from rest_framework.permissions import AllowAny
+
 schema_view = get_schema_view(
     openapi.Info(
         title="PhotoJournal API",
@@ -16,6 +18,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
+    permission_classes=[ AllowAny ],
 )
 
 urlpatterns = [
