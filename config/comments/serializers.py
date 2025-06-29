0589542799f2e6ff7959c34a0d593b37db40ebd9
0MAVM0 <ediconpts9@gmail.com>
2025-06-29 13:05:41 +0500

    Added Count On Comments Through Serializer

--- config/comments/serializers.py
+++ config/comments/serializers.py
@@ -4,8 +4,12 @@ from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
+    comments_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
-        fields = ['id', 'user', 'content', 'created_at']
-        read_only_fields = ['id', 'user', 'created_at']
+        fields = ('id', 'user', 'content', 'created_at', 'comments_count')
+        read_only_fields = ['id', 'user', 'created_at', 'comments_count']
+
+    def get_comments_count(self, obj):
+        return obj.comments.count()
