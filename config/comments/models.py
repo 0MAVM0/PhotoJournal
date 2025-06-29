661d2b1899fab5e9b0809d34c2563d4365c69cc5
0MAVM0 <ediconpts9@gmail.com>
2025-06-29 13:10:56 +0500

    Added - 'related_name' To Some Fields Of Models

--- config/comments/models.py
+++ config/comments/models.py
@@ -5,7 +5,7 @@ from posts.models import Post
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- config/posts/models.py
+++ config/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from users.models import CustomUser
 
 class Post(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(upload_to='posts/')
     caption = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
