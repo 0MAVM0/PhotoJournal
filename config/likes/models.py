4c32425d822f13b9db5ee69f2ffce5f2a70915e0
0MAVM0 <ediconpts9@gmail.com>
2025-06-30 10:28:48 +0500

    Model - Like; App - likes

--- config/likes/models.py
+++ config/likes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser
+from posts.models import Post
+
+
+class Like(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
