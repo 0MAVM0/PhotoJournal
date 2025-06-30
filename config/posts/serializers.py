2e7c8c8790f593986318f47d7dd37763e04318e7
0MAVM0 <ediconpts9@gmail.com>
2025-06-30 10:58:01 +0500

    Confusion Of Counts In Serializers

--- config/posts/serializers.py
+++ config/posts/serializers.py
@@ -4,8 +4,23 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
     image = serializers.ImageField(required=True)
+    comments_count = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+    is_liked_by_me = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ('id', 'user', 'image', 'caption', 'created_at')
-        read_only_fields = ['id', 'user', 'created_at']
+        fields = ('id', 'user', 'image', 'caption', 'created_at', 'comments_count', 'likes_count', 'is_liked_by_me')
+        read_only_fields = ['id', 'user', 'created_at', 'comments_count', 'likes_count', 'is_liked_by_me']
+    
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+    
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+    
+    def get_is_liked_by_me(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return obj.likes.filter(user=request.user).exists()
+        return False
