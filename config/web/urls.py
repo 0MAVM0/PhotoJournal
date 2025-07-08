d16d51fa934bb15e96f4b032b9cfc7c8b07405a3
0MAVM0 <ediconpts9@gmail.com>
2025-07-08 18:44:08 +0500

    Url For LoginPageView

--- config/web/urls.py
+++ config/web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterPageView
+from .views import RegisterPageView, LoginPageView
 
 urlpatterns = [
     path('register/', RegisterPageView.as_view(), name='register'),
+    path('login/', LoginPageView.as_view(), name='login'),
 ]
