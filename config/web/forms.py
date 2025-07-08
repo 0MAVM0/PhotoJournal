797738c7e93f8238677023d1e6fdce92f34cbe7e
0MAVM0 <ediconpts9@gmail.com>
2025-07-08 18:17:25 +0500

    forms.py; App - web

---
+++ config/web/forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+from django import forms
+
+User = get_user_model()
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, validators=[validate_password])
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2', 'bio', 'avatar')
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username is already taken.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already registered.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get('password') != cleaned_data.get('password2'):
+            self.add_error('password2', "Passwords do not match.")
+        return cleaned_data
