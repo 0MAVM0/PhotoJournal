b5661ef915b8a41807ec6aabedf85700210a3428
0MAVM0 <ediconpts9@gmail.com>
2025-07-08 18:43:17 +0500

    LoginPageView

--- config/web/views.py
+++ config/web/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
 from django.views import View
 
 from .forms import RegisterForm
+from posts.models import Post
 
 
 class RegisterPageView(View):
@@ -27,3 +31,14 @@ class RegisterPageView(View):
             messages.error(request, 'Please fix the errors below.')
 
         return render(request, 'register.html', { 'form': form })
+
+
+class LoginPageView(LoginView):
+    template_name = 'login.html'
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password.')
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return redirect('home')
