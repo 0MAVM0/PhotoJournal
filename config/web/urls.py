6d78cf4f6084803cb6b75196caa9c727da61ac6d
0MAVM0 <ediconpts9@gmail.com>
2025-07-08 19:25:05 +0500

    Url For CreatePostView

--- config/web/urls.py
+++ config/web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, RegisterPageView, LoginPageView
+from .views import HomeView, RegisterPageView, LoginPageView, CreatePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('register/', RegisterPageView.as_view(), name='register'),
     path('login/', LoginPageView.as_view(), name='login'),
+    path('create/', CreatePostView.as_view(), name='create-post'),
 ]
