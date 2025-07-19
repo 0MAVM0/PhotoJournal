04a8fe296518a7dad36aa0d25256cb2e065b19a3
0MAVM0 <ediconpts9@gmail.com>
2025-07-19 14:29:49 +0500

    Model - Follow; App - follows

--- config/follows/models.py
+++ config/follows/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(CustomUser, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('follower', 'following')
