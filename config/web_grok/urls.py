f5b2e02ea3f88f8c1c94baea2e9ae2b0d88a6e5d
0MAVM0 <ediconpts9@gmail.com>
2025-07-19 15:20:23 +0500

    Urls Of Web_Grok; Partial

--- config/web_grok/urls.py
+++ config/web_grok/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
-from .views import *
+from . import views
 
-urlpatterns = []
+app_name = 'web'
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('profile/<str:username>/', views.profile_view, name='profile'),
+    path('post/<int:post_id>/', views.post_detail_view, name='post_detail'),
+]
