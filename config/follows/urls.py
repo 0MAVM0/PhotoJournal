4ec5d981ee6d8d8a5e447634e2fe2ae1f735b01f
0MAVM0 <ediconpts9@gmail.com>
2025-07-19 14:32:34 +0500

    Urls Of Follows

--- config/follows/urls.py
+++ config/follows/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import FollowUserView, FollowersListView, FollowingListView
 
-urlpatterns = []
+urlpatterns = [
+    path('<uuid:user_id>/follow/', FollowUserView.as_view(), name='follow-user'),
+    path('<uuid:user_id>/followers/', FollowersListView.as_view(), name='followers-list'),
+    path('<uuid:user_id>/following/', FollowingListView.as_view(), name='following-list'),
+]
