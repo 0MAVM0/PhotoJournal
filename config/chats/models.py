bb53d17a58961bbc66be3fb5f00022f4bc1f9dd9
0MAVM0 <ediconpts9@gmail.com>
2025-07-19 14:36:31 +0500

    Models - Chat & Message

--- config/chats/models.py
+++ config/chats/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(CustomUser, related_name='chats')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Chat {self.id}"
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(CustomUser, related_name='sent_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} at {self.timestamp}"
